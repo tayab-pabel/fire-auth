dfe02216f0e968e6881265cb463d49a02fe34c2f
tayab-pabel <tayabpabel@yahoo.com>
2020-09-12 20:18:30 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0d8bWM-FFCv8VL5d8wxgw33hszlIwbyI",
+    authDomain: "tayab-pabel.firebaseapp.com",
+    databaseURL: "https://tayab-pabel.firebaseio.com",
+    projectId: "tayab-pabel",
+    storageBucket: "tayab-pabel.appspot.com",
+    messagingSenderId: "473628047145",
+    appId: "1:473628047145:web:8c6c0ecf28a732b4c1a938",
+    measurementId: "G-QRE2CWQL4K"
+  };
+
+  export default firebaseConfig;
+  
